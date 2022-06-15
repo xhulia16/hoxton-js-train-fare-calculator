f895c26cd2c91657558e85ca9028b4fd86ef943e
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-15 22:40:54 +0200

    update on excercise

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-let age =prompt("What is your age?")
-let distance=prompt("What distance(km) are you travelling?")
+let age = Number(prompt("What is your age?"))
+let distance = Number(prompt("What distance(km) are you travelling?"))
 
-if(age<18){
-    ticketPrice=(distance*0.21)*0.8
+let pricePerKm = 0.21
+let juniorDiscountRate = 0.2
+let seniorDiscountRate = 0.4
+
+let ticketPrice = distance * pricePerKm
+let juniorTicketPrice = ticketPrice - (ticketPrice * juniorDiscountRate)
+let seniorTicketPrice = ticketPrice - (ticketPrice * seniorDiscountRate)
+
+if (age < 18) {
+    console.log(
+        `
+        You get the Junior Discount!
+        You need to pay: £${juniorTicketPrice.toFixed(2)}
+          `,
+    )
 }
-else if(age>65){
-    ticketPrice=(distance*0.21)*0.6
+else if (age < 18) {
+    console.log(
+        `
+        You get the Senior Discount!
+        You need to pay: £${seniorTicketPrice.toFixed(2)}
+          `,
+    )
 }
-else{
-    ticketPrice=distance*0.21  
+else {
+    console.log(
+        `
+            You need to pay: £${ticketPrice.toFixed(2)}
+              `,
+    )
 }
-console.log("Your ticket price is", ticketPrice.toFixed(1), "pounds")
